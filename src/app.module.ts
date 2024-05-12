2c71d296279e5de9661378812fe2c56825b43552
Benoit Mirete <benoit@groover.co>
2024-05-12 09:20:19 +0200

    chore: add swagger and refacto main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { BrandsModule } from './brands/brands.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CommonModule, BrandsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CommonModule,
+    BrandsModule,
+  ],
   controllers: [],
   providers: [],
 })
